3487145b200d15fba24a01de82280036d59944a4
rushyn <tony_alexandrov@hotmail.com>
2024-09-30 20:57:26 +1000

    added staticcheck

--- main.go
+++ main.go
@@ -23,8 +23,7 @@ type apiConfig struct {
 
 //go:embed static/*
 var staticFiles embed.FS
-
-func main() {
+func main(){
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
@@ -95,4 +94,11 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+
+
+func unused() {
+    // this function does nothing
+    // and is called nowhere
 }
